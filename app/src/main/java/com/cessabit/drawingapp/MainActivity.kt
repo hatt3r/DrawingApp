187c1f79b7db8c310b8dace3bd8edc6e88da04dd
Serene <serene719@gmail.com>
2023-11-30 22:56:11 +0530

    📈 adding color pallete to UI

--- app/src/main/java/com/cessabit/drawingapp/MainActivity.kt
+++ app/src/main/java/com/cessabit/drawingapp/MainActivity.kt
@@ -1,26 +1,38 @@
 package com.cessabit.drawingapp
 
+import android.annotation.SuppressLint
 import android.app.Dialog
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.LinearLayout
+import androidx.core.content.ContextCompat
+import androidx.core.view.get
 
 class MainActivity : AppCompatActivity() {
 
     private var drawingView: DrawingView? = null
+    private var mImageCurrentPaint: ImageButton? = null
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         drawingView = findViewById(R.id.drawing_view)
         drawingView?.setSizeForBrush(20.toFloat())
 
+        val linearLayoutPaintColors = findViewById<LinearLayout>(R.id.ll_paint_colors)
+        mImageCurrentPaint = linearLayoutPaintColors[1] as ImageButton
+        mImageCurrentPaint!!.setImageDrawable(ContextCompat.getDrawable(this,R.drawable.pallete_pressed))
+
         val ib_brush: ImageButton = findViewById(R.id.ib_brush)
-        ib_brush.setOnClickListener{showBrushSizeChooserDialog()}
+        ib_brush.setOnClickListener { showBrushSizeChooserDialog() }
+
     }
 
-    private fun showBrushSizeChooserDialog(){
+    private fun showBrushSizeChooserDialog() {
         var brushDialog = Dialog(this)
         brushDialog.setContentView(R.layout.dialog_brush_size)
         brushDialog.setTitle("Brush Size: ")
