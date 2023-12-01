b2cbf6f625aed8bb1cbb8882e3aa45c4363dc68c
Serene <serene719@gmail.com>
2023-12-01 10:24:42 +0530

    📈 Adding image Gallery button

--- app/src/main/java/com/cessabit/drawingapp/MainActivity.kt
+++ app/src/main/java/com/cessabit/drawingapp/MainActivity.kt
@@ -18,7 +18,7 @@ class MainActivity : AppCompatActivity() {
     private var drawingView: DrawingView? = null
     private var mImageCurrentPaint: ImageButton? = null
 
-    @SuppressLint("MissingInflatedId")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
